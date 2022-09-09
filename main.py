4e231a617df32540926a541051083ca58751d6b4
2662180776 <112801980+2662180776@users.noreply.github.com>
2022-09-10 02:00:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -180,8 +180,10 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         birth_day = get_birthday(value["birthday"], year, today)
         if birth_day == 0:
             birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
-        else:
-            birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day1)
+       if else:
+            birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
+          else:
+            birthday_data = "距离{}还有{}天".format(value["name"], birth_day)
         # 将生日数据插入data
         data["data"][key] = {"value": birthday_data, "color": get_color()}
     headers = {
